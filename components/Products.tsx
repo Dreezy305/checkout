a8e5cbe60e04982a2c43d47ec24141b4be340b6f
Dreezy305 <bankoleidris@gmail.com>
2023-01-08 21:45:15 +0100

    checkout, product page, component

--- components/Products.tsx
+++ components/Products.tsx
@@ -24,21 +24,26 @@ function Product(): JSX.Element {
         sx={{ display: "flex", flexDirection: "row" }}
         className="py-3 items-start justify-between"
       >
-        <Checkbox
-          checked={checked}
-          onChange={handleChange}
-          inputProps={{ "aria-label": "controlled" }}
-        />
-        <CardMedia
-          component="img"
-          sx={{ width: 151 }}
-          image="https://res.cloudinary.com/africahacks/image/upload/v1606074022/chimoney/Purple-100_z05fgn.jpg"
-          alt="Live from space album cover"
-          className=""
-        />
-        <Box sx={{ display: "flex", flexDirection: "column" }}>hello</Box>
+        <Box
+          sx={{ display: "flex", flexDirection: "row" }}
+          className="space-x-3"
+        >
+          <Checkbox
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+          <CardMedia
+            component="img"
+            sx={{ width: 151 }}
+            image="https://res.cloudinary.com/africahacks/image/upload/v1606074022/chimoney/Purple-100_z05fgn.jpg"
+            alt="Live from space album cover"
+            className=""
+          />
+          <Box sx={{ display: "flex", flexDirection: "column" }}>hello</Box>
+        </Box>
 
-        <Typography component={"div"} variant={"h6"}>
+        <Typography component={"div"} variant={"h6"} className="float-right">
           $179.99
         </Typography>
       </Box>
